caefb9f580585b414b1bad5aec890819b537828d
nikhilbangar <nikhil.s.bangar@gmail.com>
2015-05-10 16:39:02 +0530

    Change default values, and refactor for loop.

--- SalesTool/src/com/demo/salestool/SalesData.java
+++ SalesTool/src/com/demo/salestool/SalesData.java
@@ -1,11 +1,11 @@
 package com.demo.salestool;
 
 public class SalesData {
-	int[] data = {10,20,30};
+	int[] data = {10,20,30,40,50,60};
 	
 	public void display() {
 		System.out.println("Data: ");
-		for(int i=0; i < data.length; i++){
+		for(int i:data){
 			System.out.println("Next Value: " + data[i]);
 		}
 	}
